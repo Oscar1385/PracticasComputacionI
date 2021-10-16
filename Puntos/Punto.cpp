9b6613fdcee87af2c06fe7d879ce35304c5e3a48
Oscar1385 <89151561+Oscar1385@users.noreply.github.com>
2021-10-15 21:48:14 -0500

    Update Punto.cpp

--- Puntos/Punto.cpp
+++ Puntos/Punto.cpp
@@ -1,26 +1,48 @@
 //
-// Created by Oscar Eduardo Ortega Penagos on 14/10/2021.
+// Created by Oscar Eduardo Ortega Penagos on 15/10/2021.
 //
 
-#ifndef PUNTOS_PUNTO_H
-#define PUNTOS_PUNTO_H
 #include <iostream>
+#include <math.h>
+#include <Punto.h>
+#define PI 3.14159265
 
 using namespace std;
 
-class Punto2D{
-public:
-    float x, y;
-    void SetPosicion(double a, double b);
-    void Trasladar (float temp1, float temp2);
-    void RotarRespectoAlOrigen (float temp1);
-    void Escalar (float temp1, float temp2);
-    float GetX();
-    float GetY();
+Punto2D::Punto2D(){
+    a = 0;
+    b = 0;
+    c = 0;
+}
 
-private:
-    float a, b, c;
+void Punto2D::SetPosicion(double a, double b) {
+    a = x
+    b = y
+}
 
-};
+void Punto2D::Trasladar(float temp1, float temp2) {
+    a = a + temp1;
+    b = b + temp2;
+}
 
-#endif //PUNTO_PUNTO_H
+void Punto2D::RotarRespectoAlOrigen(float temp1) {
+    c = a;
+    a = c*cos(temp1*PI/180) - b*sin(temp1*PI/180)
+    b = c*sin(temp1*PI/180) + b*cos(temp1*PI/180)
+}
+
+void Punto2D::Escalar(float temp1, float temp2) {
+    a = a * temp1;
+    b = b * temp2;
+}
+
+float Punto2D::GetX() {
+    return a;
+}
+
+float Punto2D::GetY() {
+    return b;
+}
+
+
+
