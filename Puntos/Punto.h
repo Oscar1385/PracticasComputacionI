d2b6811b2a6666b32e9e9d4a19426ad394f66c6b
Oscar1385 <89151561+Oscar1385@users.noreply.github.com>
2021-10-15 21:49:40 -0500

    Update Punto.h

--- Puntos/Punto.h
+++ Puntos/Punto.h
@@ -1,1 +1,26 @@
+//
+// Created by Oscar Eduardo Ortega Penagos on 14/10/2021.
+//
 
+#ifndef PUNTOS_PUNTO_H
+#define PUNTOS_PUNTO_H
+#include <iostream>
+
+using namespace std;
+
+class Punto2D{
+public:
+    float x, y;
+    void SetPosicion(double a, double b);
+    void Trasladar (float temp1, float temp2);
+    void RotarRespectoAlOrigen (float temp1);
+    void Escalar (float temp1, float temp2);
+    float GetX();
+    float GetY();
+
+private:
+    float a, b, c;
+
+};
+
+#endif //PUNTO_PUNTO_H
